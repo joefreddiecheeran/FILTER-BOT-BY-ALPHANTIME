f71197d2c01b1e627b1343d6138b8884d75b61cc
joefreddiecheeran <70432335+joefreddiecheeran@users.noreply.github.com>
2021-04-20 19:04:24 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,4 @@
 class Script(object):
 
     START_MSG = """ <i><b>എന്താ മോനൂസെ ജാഡയാണൊ  🤣🤣</b></i>
-
-Pm @alphantime to make a bot like this for you. If you are limited, contact him through @AlphantimeBot.
-This bot is maintained by @Fabio_TG..
 """
